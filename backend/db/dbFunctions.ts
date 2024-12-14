d843d4979dacd6fe1a19b3f8661ae5845b7a0baf
JaninaMel <janina.elijoki@gmail.com>
2024-12-14 15:57:43 +0200

    Add dbFunctions.js for handlind the database

---
+++ backend/db/dbFunctions.ts
@@ -0,0 +1,38 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database(":memory:", (err) => {
+    if (err) {
+        console.error("Error connecting to database");
+    } else {
+        console.log("Connected to database");
+    }
+})
+
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY AUTOINCREMENT, english VARCHAR, finnish VARCHAR);");
+    db.run("INSERT INTO words (english, finnish) VALUES (?, ?)", ["Dog", "Koira"]);
+    db.run("INSERT INTO words (english, finnish) VALUES (?, ?)", ["Cat", "Kissa"]);
+    db.run("INSERT INTO words (english, finnish) VALUES (?, ?)", ["Donkey", "Aasi"]);
+    db.all("SELECT * FROM words", [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        console.log(rows);
+    })
+})
+
+const databaseFunctions = {
+    findAll: () => {
+        return new Promise((resolve, reject) => {
+            db.all("SELECt * FROM words", (err, results) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(results);
+            })
+        })
+    }
+}
+
+module.exports = databaseFunctions;
