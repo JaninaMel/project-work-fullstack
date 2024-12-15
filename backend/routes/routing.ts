e940b69d98d08a07b40d1846bfbfcf995b97693c
JaninaMel <janina.elijoki@gmail.com>
2024-12-15 05:23:13 +0200

    Add handling for http post requests

--- backend/routes/routing.ts
+++ backend/routes/routing.ts
@@ -2,6 +2,7 @@ const db = require("../db/dbFunctions.ts");
 const express = require('express');
 const wordRouter = express.Router();
 
+// Fetches all word pairs from database.
 wordRouter.get("/", async (req, res) => {
     try {
         const words = await db.findAll();
@@ -9,6 +10,16 @@ wordRouter.get("/", async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(404).send("Could not find anything.");
+    }
+})
+
+//Inserts new word pairs into the database.
+wordRouter.post("/", async (req, res) => {
+    try {
+        const wordPair = await db.addNew(req.body);
+        res.status(201).json(wordPair);
+    } catch (error) {
+        res.status(400).json(error);
     }
 })
 
