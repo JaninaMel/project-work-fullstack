50b7276ec784e1bff8b06196695459322de14036
JaninaMel <janina.elijoki@gmail.com>
2025-01-09 23:17:25 +0200

    UserComponent: Changed handleSubmit functions answer checking to account for ids not matching indexes in arrays. Added JSDoc comments.

--- frontend/src/UserComponent.jsx
+++ frontend/src/UserComponent.jsx
@@ -1,6 +1,18 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+// TODO: possibly add a list for the words users got wrong.
+
+/**
+ * A component for users to train the words in the database,
+ * by providing finnish translations to the given english words.
+ *
+ * Includes and array with input fields where the user is able to
+ * give their answer and a button to submit the user's answer which
+ * then reveals them their score.
+ *
+ * @returns {JSX.Element} for users to train the word pairs and get their results.
+ */
 function UserComponent() {
     // Array for the word pairs fetched from backend.
     let [words, setWords] = useState([]);
@@ -10,45 +22,70 @@ function UserComponent() {
     let [score, setScore] = useState(null);
     const apiUrl = `/api/words`;
 
+    // Invokes fetchWords function when the page is loaded.
     useEffect(() => {
-        const fetchIt = async () => {
+        const fetchWords = async () => {
             const hr = await axios.get(apiUrl);
             let data = hr.data;
             setWords(data);
         }
 
-        fetchIt();
+        fetchWords();
     }, []);
 
-    // Handles changes made into the input fields by the user.
+    /**
+     * Handles changes made into the input fields for answer by the user.
+     *
+     * @param {number} id The id of the word the user is giving the answer to.
+     *                    Set as the key for the key value pair.
+     * @param {String} ans The answer given by the user. From the event object
+     *                     of the  on change event of the input field. Set as the
+     *                     value in the key value pair.
+     */
     const handleUserAnsChange = (id, ans) => {
-        setAnswers((prevAnswers) => ({
-            ...prevAnswers, [id]: ans
-        }))
+        setAnswers((prevAnswers) => ({...prevAnswers, [id]: ans}));
     }
 
-    // Handles clicking the submit button.
-    // User gets a score based on how many they got correct.
+    /**
+     * Handles checking the answer and counting the score when
+     * user clicks the submit button.
+     * Compares the correct finnish translation from the database
+     * with the user's input. Ignores case.
+     * Calls the setState for the score and updates it when the answer is correct.
+     */
     const handleSubmit = () => {
         let scoreCount = 0;
-        if (answers === undefined || answers.length == 0) {
-            return;
-        }
+        let ansKeys = Object.keys(answers);
+        // Parsing the numbers in the key array.
+        let parsedKeys = ansKeys.map(key => parseInt(key, answers.length));
+
         for (let i = 0; i < words.length; i++) {
-            if (answers[i+1] !== undefined && words[i].finnish.toLowerCase() === answers[i+1].toLowerCase()) {
+            if (answers[parsedKeys[i]] !== undefined && words.find(answer => answer.id === parsedKeys[i]).finnish.toLowerCase() === answers[parsedKeys[i]].toLowerCase()) {
                 scoreCount++;
             }
             setScore(scoreCount);
         }
     }
 
+    // Mapping the array for the table element incliding the anser input fields.
     let arr = words.map((word) => (
         <tr key={word.id}>
             <td>{word.english}</td><td><input type='text' onChange={(e) => handleUserAnsChange(word.id, e.target.value)}></input></td>
         </tr>
     ))
+
     return (
         <div id="userView">
+            <div className='instructions'>
+                <h1>Instructions</h1>
+                <p>Answer into the fields provided in
+                    the "Finnish" column. Answer with
+                    the Finnish translation of the word provided
+                    in English in the "English" column. Once you're done,
+                    click the "Submit" button and you will get your score.
+                </p>
+                <p>You can leave fields empty.</p>
+            </div>
     <table>
         <thead>
         <tr>
