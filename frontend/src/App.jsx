0866bf110a95c9a73c7e162812fcc3211e35bc9e
JaninaMel <janina.elijoki@gmail.com>
2025-01-13 16:59:46 +0200

    App: Add route for UserComponentAlt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import UserComponent from './UserComponent';
 import AdminComponent from './AdminComponent';
 import EditComponent from './EditComponent';
+import UserComponentAlt from './UserComponentAlt';
 import './App.css';
 
 /**
@@ -26,6 +27,7 @@ function App() {
       </>
       <Routes>
         <Route path='/' element={<UserComponent />}></Route>
+        <Route path='/alt' element={<UserComponentAlt />}></Route>
         <Route path='admin/' element={<AdminComponent />}></Route>
         <Route path='edit/:wordId' element={<EditComponent />}></Route>
       </Routes>
